ee7e2558d0cde300e1259f9c627ddaaa5b0746df
James S <i@nixeagle.org>
2010-05-05 20:33:44 -0400

    Start messing with the MOP and getting a concise description of binary formats.

--- binary-data.lisp
+++ binary-data.lisp
@@ -1,11 +1,10 @@
-`(defpackage #:nixeagle.binary-data
-   (:use :cl :closer-mop)
-   (:nicknames :binary-data)
-   (:shadowing-import-from :closer-mop
-                           ,@'#.(let (list)
-                                  (do-external-symbols (s :closer-mop)
-                                    (push s list))
-                                  list)))
+(defpackage #:nixeagle.binary-data
+  (:use :cl :closer-mop)
+  (:nicknames :binary-data)
+  #.(let (list)
+      (do-external-symbols (s :closer-mop)
+        (push s list))
+      `(:shadowing-import-from :closer-mop ,@list)))
 
 (in-package :nixeagle.binary-data)
 
@@ -15,6 +14,26 @@
 This will be used in generic functions and method specializers as the base
 class. All classes have to be compatable with these methods or implement
 modifications so they do the right thing."))
+
+(defclass primitive-binary (binary)
+  ((value :type (integer 10 20))
+   size)
+  (:documentation "Simple binary types such as integers."))
+
+(defclass bit-field-slot-definition (standard-slot-definition)
+  ((bit-field-size :accessor bit-field-size :initarg :bits
+                   :initform nil)))
+
+(defclass bit-field-direct-slot-definition (standard-direct-slot-definition
+                                            bit-field-slot-definition)
+   ())
+
+(defclass bit-field-effective-slot-definition (standard-effective-slot-definition
+                                               bit-field-slot-definition)
+   ())
+
+(defclass bit-field-metaclass (standard-class)
+  ())
 
 
 ;;; END
