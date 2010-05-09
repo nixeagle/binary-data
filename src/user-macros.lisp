cfbafc35f7e0c164cfa32399d78b56fa1e56fa3d
James S <i@nixeagle.org>
2010-05-08 21:44:28 -0400

    docstring for DEFINE-BINARY-CLASS.

--- src/user-macros.lisp
+++ src/user-macros.lisp
@@ -1,6 +1,10 @@
 (in-package :binary-data)
 
 (defmacro define-binary-class (name superclasses slots &rest options)
+  "Defines a BINARY class in the same manner defclass would.
+
+This macro is not api stable, expect breakage for a few days.
+  -- nixeagle [2010-05-08 Sat 21:40]"
   `(defclass ,name ,superclasses ,slots
      ,@(if (find :metaclass options :key #'car)
           options                     ;leave as is
