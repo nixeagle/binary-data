f781ee9022727c997e3b035e1ba12f75d07eb509
James S <i@nixeagle.org>
2010-05-09 03:20:46 -0400

    Export all the important symbols to binary-data
    This is a must look commit!. Will tag v0.1

--- package.lisp
+++ package.lisp
@@ -7,4 +7,20 @@
       (do-external-symbols (s :closer-mop)
         (push s list))
       `(:shadowing-import-from :closer-mop ,@list))
-  (:export #:define-binary-class))
+  (:export #:define-binary-class
+           #:big-endian
+           #:little-endian
+           #:endian-mixin
+           #:bit-size-of
+           #:size-of
+           #:compute-slot-positions
+           #:compute-endian-slot-positions
+           #:primary-machine-byte-size
+           #:write-object
+           #:write-octets
+           #:read-object
+           #:binary-slot-value
+           #:binary-data-object
+           #:binary-data-metaclass))
+
+;;; END
